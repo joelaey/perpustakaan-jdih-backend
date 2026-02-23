02e0f927060cf575482f74fda426abbf1db8e166
joelaey <mjlynsyh@gmail.com>
2026-02-24 02:01:37 +0700

    Fix : JWT Generate failure

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -104,7 +104,7 @@ const login = async (req, res) => {
 
         // Generate token
         const token = jwt.sign(
-            { id: user.id, name: user.name, email: user.email, role: user.role, avatar: user.avatar },
+            { id: user.id },
             JWT_SECRET,
             { expiresIn: '7d' }
         );
